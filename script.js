61a7fa56f4bd0ff4b3b7445ec690cf81ec9ab5a3
Roman Ponepoliak <romanponepolyak@gmail.com>
2024-01-29 15:47:17 +0100

    29.01.24

--- script.js
+++ script.js
@@ -39,17 +39,3 @@ if (window.innerWidth <= 960) {
   document.getElementById('last-article-text').start = 3;
   document.getElementById('third-article-text').start = 4;
 }
-
-document.addEventListener('DOMContentLoaded', function () {
-  var buttons = document.querySelectorAll('.my-button');
-
-  buttons.forEach(function (button) {
-      button.addEventListener('touchstart', function () {
-          this.classList.add('active');
-      });
-
-      button.addEventListener('touchend', function () {
-          this.classList.remove('active');
-      });
-  });
-});
