d67ef0cd1a8a326b33900d9f866fdee4ffbffb44
Roman Ponepoliak <romanponepolyak@gmail.com>
2024-02-11 14:59:49 +0100

    Reviews mobile

--- reviews/reviews.js
+++ reviews/reviews.js
@@ -1,11 +1,19 @@
 const swiper = new Swiper('.swiper', {
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-});
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 
-  
-  
-  
+  // Default parameters
+  slidesPerView: 1,
+  slidesPerGroup: 1, // Number of slides to scroll by
+
+  breakpoints: {
+    // when window width is >= 820px
+    821: {
+      slidesPerView: 2,
+      slidesPerGroup: 2, // Number of slides to scroll by
+    },
+  }
+});
